5f4340f34762593e60e6909d2a09df9b5b42b876
Popenkov Anton <popenkovaa@yandex.ru>
2022-08-07 18:28:30 +0300

    validation on register added

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+import { registerValidation } from './validations/auth.js';
+
+mongoose
+  .connect(
+    'mongodb+srv://admin:9lEjqiXNwICdjFCt@cluster0.bps3ayw.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('db ok');
+  })
+  .catch((error) => {
+    console.log('db error', error);
+  });
 
 //записываем всю логику экспресса в app
 const app = express();
@@ -29,6 +43,16 @@ app.post('/auth/login', (req, res) => {
   res.json({
     success: true,
     token,
+  });
+});
+
+app.post('/auth/register', registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array);
+  }
+  res.json({
+    success: true,
   });
 });
 
